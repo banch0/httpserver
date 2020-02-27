2d74e64970beba4478b1ec2acd54e6fea1a6e922
Behruz Valiev <behruzvaliev@gmail.com>
2020-02-27 17:35:56 +0500

    fix: link error

--- cmd/httpserve/main_test.go
+++ cmd/httpserve/main_test.go
@@ -1,22 +1,267 @@
 package main
 
 import (
+	"log"
+	"net"
 	"net/http"
-	"net/http/httptest"
 	"testing"
+	"time"
 )
 
-func TestHandleFile(t *testing.T) {
-	dir := http.Dir("./assets/")
+func start(host string) {
+	listener, err := net.Listen("tcp", "0.0.0.0:9999")
+	if err != nil {
+		log.Fatalf("Servet can't started error: %v", err)
+	}
+	defer listener.Close()
 
-	// strip prefix instead /temp/*.jpg /*.jpg
-	handler := http.StripPrefix("/assets", http.FileServer(dir))
-	mux := http.NewServeMux()
-	mux.Handle("/assets/", handler)
-	writer := httptest.NewRecorder()
-	request, _ := http.NewRequest("GET", "/assets/", nil)
-	mux.ServeHTTP(writer, request)
-	// if writer.Code != 200 {
-	// 	t.Errorf("Response code is %v", writer.Code)
-	// }
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("can't accept client: ", err)
+		}
+		go requestHandler(conn)
+	}
+}
+
+var netClient = &http.Client{
+	Timeout: 50 * time.Second,
+}
+
+var link = "http://127.0.0.1:9999"
+
+func Test_server(t *testing.T) {
+	// naive approach
+	go func() {
+		listener, err := net.Listen("tcp", "0.0.0.0:9999")
+		if err != nil {
+			log.Fatalf("Servet can't started error: %v", err)
+		}
+		defer func() {
+			err := listener.Close()
+			if err != nil {
+				t.Logf("Listener close error: %v", err)
+			}
+		}()
+
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				log.Println("can't accept client: ", err)
+			}
+			go requestHandler(conn)
+		}
+	}()
+
+	go func() {
+		httpRequest, err := http.NewRequest("GET", link, nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		resp, err := netClient.Do(httpRequest)
+		if err != nil {
+			t.Error(err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != 200 {
+			t.Error(err)
+		}
+		time.Sleep(5 * time.Second)
+	}()
+}
+
+func TestDownload(t *testing.T) {
+	go func() {
+		listener, err := net.Listen("tcp", "0.0.0.0:9999")
+		if err != nil {
+			log.Fatalf("Servet can't started error: %v", err)
+		}
+		defer func() {
+			err := listener.Close()
+			if err != nil {
+				t.Logf("Listener close error: %v", err)
+			}
+		}()
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				log.Println("can't accept client: ", err)
+			}
+			go requestHandler(conn)
+		}
+	}()
+
+	go func() {
+		httpRequest, err := http.NewRequest("GET", link, nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		resp, err := netClient.Do(httpRequest)
+		if err != nil {
+			t.Error(err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 200 {
+			t.Error(err)
+		}
+	}()
+}
+
+func TestImage(t *testing.T) {
+	go func() {
+		listener, err := net.Listen("tcp", "0.0.0.0:9999")
+		if err != nil {
+			log.Fatalf("Servet can't started error: %v", err)
+		}
+		defer func() {
+			err := listener.Close()
+			if err != nil {
+				t.Logf("Listener close error: %v", err)
+			}
+		}()
+
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				log.Println("can't accept client: ", err)
+			}
+			go requestHandler(conn)
+		}
+	}()
+
+	go func() {
+		httpRequest, err := http.NewRequest("GET", link+"/assets/image.jpg", nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		resp, err := netClient.Do(httpRequest)
+		if err != nil {
+			t.Error(err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != 200 {
+			t.Error(err)
+		}
+	}()
+}
+
+func TestImagePng(t *testing.T) {
+	go func() {
+		listener, err := net.Listen("tcp", "0.0.0.0:9999")
+		if err != nil {
+			log.Fatalf("Servet can't started error: %v", err)
+		}
+		defer func() {
+			err := listener.Close()
+			if err != nil {
+				t.Logf("Listener close error: %v", err)
+			}
+		}()
+
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				log.Println("can't accept client: ", err)
+			}
+			go requestHandler(conn)
+		}
+	}()
+
+	go func() {
+		httpRequest, err := http.NewRequest("GET", link+"/assets/image.png", nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		resp, err := netClient.Do(httpRequest)
+		if err != nil {
+			t.Error(err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != 200 {
+			t.Error(err)
+		}
+	}()
+}
+
+func TestPdf(t *testing.T) {
+	go func() {
+		listener, err := net.Listen("tcp", "0.0.0.0:9999")
+		if err != nil {
+			log.Fatalf("Servet can't started error: %v", err)
+		}
+		defer func() {
+			err := listener.Close()
+			if err != nil {
+				t.Logf("Listener close error: %v", err)
+			}
+		}()
+
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				log.Println("can't accept client: ", err)
+			}
+			go requestHandler(conn)
+		}
+	}()
+
+	go func() {
+		httpRequest, err := http.NewRequest("GET", link+"/assets/doc", nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		resp, err := netClient.Do(httpRequest)
+		if err != nil {
+			t.Error(err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != 200 {
+			t.Error(err)
+		}
+	}()
+}
+
+func TestTXT(t *testing.T) {
+	go func() {
+		listener, err := net.Listen("tcp", "0.0.0.0:9999")
+		if err != nil {
+			log.Fatalf("Servet can't started error: %v", err)
+		}
+		defer func() {
+			err := listener.Close()
+			if err != nil {
+				t.Logf("Listener close error: %v", err)
+			}
+		}()
+
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				log.Println("can't accept client: ", err)
+			}
+			go requestHandler(conn)
+		}
+	}()
+
+	go func() {
+		httpRequest, err := http.NewRequest("GET", link+"/assets/text", nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		resp, err := netClient.Do(httpRequest)
+		if err != nil {
+			t.Error(err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != 200 {
+			t.Error(err)
+		}
+	}()
 }
